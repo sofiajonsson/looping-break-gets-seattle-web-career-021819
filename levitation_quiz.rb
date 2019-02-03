2f9c22da9ec091d6e87de6c9f54431710cda5640
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 01:11:42 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,15 @@
 
 def levitation_quiz
+  loop do
+    puts "What"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
 	#your code here
 end
+puts ""
+
+end 
+
+levitation_quiz
 
 
